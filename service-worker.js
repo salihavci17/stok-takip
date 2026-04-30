60f20cd2864bd811f236fe7653f4497755b77c10
AVCI YAZILIM <salihavci17@gmail.com>
2026-04-30 14:41:47 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,27 @@
-const CACHE_NAME = 'stok-v2'; // v1'den v2'ye yükselttik
+const CACHE_NAME = 'stok-v2';
 const assets = [
     './', 
     './index.html', 
     './style.css', 
     './app.js',
-    './manifest.json' // Manifest'i de önbelleğe ekleyelim
+    './manifest.json',
+    'https://cdn-icons-png.flaticon.com/512/2897/2897785.png'
 ];
 
+// Yükleme ve Önbelleğe Alma
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(assets);
+        })
+    );
+});
+
+// ÖNEMLİ: İstekleri Önbellekten Sunma (Bu kısım sende eksikti)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
         })
     );
 });
