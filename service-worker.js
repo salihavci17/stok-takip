4d3e507eabe2b29a03429caca207661d38b78a0d
AVCI YAZILIM <salihavci17@gmail.com>
2026-04-30 14:23:29 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,18 @@
-self.addEventListener('fetch', function(event) {
-    // Bu kısım uygulamanın hızlı açılmasını sağlar
-});
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Kuruldu');
+const CACHE_NAME = 'stok-v1';
+const assets = ['./', './index.html', './style.css', './app.js'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Bu boş olsa bile tanımlanmış olması PWA için yeterlidir
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
