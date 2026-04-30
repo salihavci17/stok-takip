97021993203a8bab7f867d13c4d36aa5dea4cb36
AVCI YAZILIM <salihavci17@gmail.com>
2026-04-30 14:13:28 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', function(event) {
     // Bu kısım uygulamanın hızlı açılmasını sağlar
 });
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Kuruldu');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Bu boş olsa bile tanımlanmış olması PWA için yeterlidir
+});
